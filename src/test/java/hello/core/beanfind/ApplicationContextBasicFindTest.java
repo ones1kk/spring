7076a58cea32c175f543fd51bc750a0321c6a2f6
ones1kk <kwsic@naver.com>
2022-02-08 23:30:56 +0900

    🔥 Retry : Find beans

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -1,11 +1,11 @@
 package hello.core.beanfind;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
@@ -23,7 +23,7 @@ public class ApplicationContextBasicFindTest {
     }
 
     @Test
-    @DisplayName("이름 없이 타입으로만 조회")
+    @DisplayName("타입으로 조회")
     void findBeanByType() {
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
@@ -37,11 +37,11 @@ public class ApplicationContextBasicFindTest {
     }
 
     @Test
-    @DisplayName("빈 이름으로 조회X")
+    @DisplayName("빈 이름으로 조회 X")
     void findBeanByNameX() {
-//        MemberService xxxxx = ac.getBean("xxxxx", MemberService.class);
-        assertThrows(NoSuchBeanDefinitionException.class,
-            () -> ac.getBean("xxxxx", MemberService.class));
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+            () -> ac.getBean("xxxx", MemberService.class));
     }
 
+    
 }
