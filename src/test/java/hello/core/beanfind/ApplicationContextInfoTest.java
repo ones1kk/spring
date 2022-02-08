b71341679dcebefb0d08bd61e79719e6aba2f2c9
ones1kk <kwsic@naver.com>
2022-02-08 23:13:40 +0900

    🔥 Retry : Find spring beans

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -16,7 +16,7 @@ class ApplicationContextInfoTest {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("beanDefinitionName = " + beanDefinitionName + " object = " + bean);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
         }
     }
 
@@ -27,14 +27,13 @@ class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
-            // Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
-            // Role ROlE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            // Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            // Role ROLE_INFRASTRUCTURE: 스프링 내부에서 사용하는 빈
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
-                Object bean = ac.getBean(beanDefinitionName);
-                System.out.println(
-                    "beanDefinitionName = " + beanDefinitionName + " object = " + bean);
+                System.out.println("name = " + beanDefinitionName + " object = " + beanDefinition);
             }
         }
     }
+
 
 }
