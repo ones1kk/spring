f3eea6a08d5917f0bf32a17261d917d09b7a7ec6
ones1kk <kwsic@naver.com>
2022-02-10 00:44:20 +0900

    🔥 Retry : Caution of singleton

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,15 +2,10 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-//    private int price; // 상태를 유지하는 필드
-
-//    public void order(String name, int price) {
-//        System.out.println("name = " + name + " price = " + price);
-//        this.price = price;
-//    }
+//    private int price;
 
     public int order(String name, int price) {
-        System.out.println("name = " + name + " price = " + price);
+        System.out.println("name = " + name + "price = " + price);
 //        this.price = price;
         return price;
     }
@@ -18,4 +13,7 @@ public class StatefulService {
 //    public int getPrice() {
 //        return price;
 //    }
+
+
+
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -1,5 +1,6 @@
 package hello.core.singleton;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -10,19 +11,17 @@ class StatefulServiceTest {
     void statefulServiceSingleton() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
             TestConfig.class);
+
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
-        // ThreadA : 사용자 A 10000원 주문
-        int userAPrice = statefulService1.order("userA", 10000);
-        // ThreadB : 사용자 B 10000원 주문
-        statefulService2.order("userB", 20000);
+        int userAPrice = statefulService1.order("userA", 1000);
+        int userBPrice = statefulService1.order("userB", 2000);
 
-        // ThreadA : 사용자 A 주문 금액 조회
-//        int price1 = statefulService1.getPrice();
-        System.out.println("price = " + userAPrice);
+        System.out.println("userAPrice = " + userAPrice);
+        System.out.println("userBPrice = " + userBPrice);
 
-//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        Assertions.assertThat(userAPrice).isNotEqualTo(userBPrice);
 
     }
 
