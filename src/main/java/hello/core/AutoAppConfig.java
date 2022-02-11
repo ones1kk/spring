37c0dd12cc5061c249328ec13243a40f8002281d
ones1kk <kwsic@naver.com>
2022-02-11 23:43:32 +0900

    🔥 Retry : Option

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,8 +1,5 @@
 package hello.core;
 
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,11 +11,11 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        System.out.println("AutoAppConfig.memberRepository");
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        System.out.println("AutoAppConfig.memberRepository");
+//        return new MemoryMemberRepository();
+//    }
 
 
 
--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -17,20 +17,20 @@ public class AutowiredTest {
     }
 
     static class TestBean {
+
         @Autowired(required = false)
-        public void setNoBean1(Member  member){
-            System.out.println("member1 = " + member);
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
         }
 
-        @Autowired
-        public void setNoBean2(@Nullable Member  member){
-            System.out.println("member2 = " + member);
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
         }
 
-        @Autowired
-        public void setNoBean3(Optional<Member> member){
-            System.out.println("member3 = " + member);
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
         }
+
     }
 
 }
